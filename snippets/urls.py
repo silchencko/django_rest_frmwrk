6a0e8059a4f51d62cb5d468aed966ba4194bd9d2
Kateryna Maistrenko <silchencko@gmail.com>
2023-09-03 15:15:15 +0300

    implement Django REST framework tutorial

---
+++ snippets/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename="snippet")
+router.register(r'users', views.UserViewSet, basename="user")
+
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
